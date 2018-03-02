aa0299107c87fce1cba1b188debd0fccc4e61a64
Bruna <brunacoqui@uft.edu.br>
2018-03-02 11:18:13 -0300

    Aula 2

---
+++ JS/funcoes.js
@@ -0,0 +1,19 @@
+//console.log("Oie mundo");
+// o console é onde verifico meus erros (tipo variavel, printf na web)
+
+//alert("OPA DANADO");
+//aparece o pop up de erro
+
+function alerta(texto)
+    {
+        alert(texto);
+    }
+
+
+
+
+
+
+
+
+
